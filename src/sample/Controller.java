e811eed3a757d4fd3b548db044886132ac85bd82
danplo1 <budweiser401@wp.pl>
2018-08-30 21:49:32 +0200

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,36 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+
+
+public class Controller {
+
+    @FXML
+    private Label scoreLabel;
+
+    @FXML
+    private TextField heightField;
+
+    @FXML
+    private TextField weightField;
+
+
+    @FXML
+    private void handleButtonAction(ActionEvent event){
+        System.out.println("Kliknięto przycisk");
+
+        double height  = Integer.parseInt(heightField.getText())/100.0;
+        int weight =Integer.parseInt(weightField.getText());
+
+        double score = weight/(height*height);
+
+        scoreLabel.setText(String.format("Wskaznik BMI wynosi: %.2f",score));
+    }
+
+
+}
+
